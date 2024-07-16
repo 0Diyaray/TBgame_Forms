b0db22e5866cb8e466082876870f590d9443e3e5
Diyar AY <Diyar AY@DESKTOP-UGFFIQ5>
2024-07-16 12:41:40 +0300

    Proje dosyası ekle.

---
+++ Oyun/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Oyun
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			button1 = new Button();
+			label1 = new Label();
+			label2 = new Label();
+			pictureBox1 = new PictureBox();
+			pictureBox2 = new PictureBox();
+			label3 = new Label();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(281, 362);
+			button1.Name = "button1";
+			button1.Size = new Size(147, 76);
+			button1.TabIndex = 0;
+			button1.Text = "Attack";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(90, 32);
+			label1.Name = "label1";
+			label1.Size = new Size(42, 15);
+			label1.TabIndex = 1;
+			label1.Text = "Health";
+			label1.Click += label1_Click;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(569, 32);
+			label2.Name = "label2";
+			label2.Size = new Size(45, 15);
+			label2.TabIndex = 2;
+			label2.Text = "Goblin ";
+			label2.Click += label2_Click;
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+			pictureBox1.Location = new Point(455, 78);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(267, 246);
+			pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox1.TabIndex = 3;
+			pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+			pictureBox2.Location = new Point(12, 78);
+			pictureBox2.Name = "pictureBox2";
+			pictureBox2.Size = new Size(253, 246);
+			pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+			pictureBox2.TabIndex = 4;
+			pictureBox2.TabStop = false;
+			pictureBox2.Click += pictureBox2_Click;
+			// 
+			// label3
+			// 
+			label3.Font = new Font("Segoe UI", 20F);
+			label3.Location = new Point(271, 177);
+			label3.Name = "label3";
+			label3.Size = new Size(178, 55);
+			label3.TabIndex = 5;
+			label3.TextAlign = ContentAlignment.MiddleCenter;
+			label3.Click += label3_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = SystemColors.Control;
+			ClientSize = new Size(800, 450);
+			Controls.Add(label3);
+			Controls.Add(pictureBox2);
+			Controls.Add(pictureBox1);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(button1);
+			Name = "Form1";
+			Text = "OYON";
+			Load += Form1_Load;
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button button1;
+		private Label label1;
+		private Label label2;
+		private PictureBox pictureBox1;
+		private PictureBox pictureBox2;
+		private Label label3;
+	}
+}
---
+++ Oyun/Form1.cs
@@ -0,0 +1,115 @@
+using System.Runtime.CompilerServices;
+
+namespace Oyun
+{
+	public partial class Form1 : Form
+	{
+		Creature sam = new Creature();
+		Creature goblin = new Creature();
+		int stor = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			sam.Power = 10;
+			sam.Health = 100;
+			goblin.Power = 10;
+			goblin.Health = 100;
+			sam.Health_Potion = true;
+		}
+		private void DisableControls()
+		{
+			foreach (Control control in Controls)
+			{
+				control.Enabled = false;
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			goblin.Health -= sam.attack();
+			if (goblin.Health <= 0)
+			{
+				label3.Text = "KAZANDIN";
+				DisableControls();
+			}
+			else
+			{
+				label2.Text = Convert.ToString(goblin.Health);
+			}
+			sam.Health -= goblin.attack();
+			if (sam.Health <= 0)
+			{
+				label3.Text = "KAYBETTÝN";
+				DisableControls();
+			}
+			else
+			{
+				label1.Text = Convert.ToString(sam.Health);
+			}
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+			if (sam.Health_Potion == true)
+			{
+				sam.Health += 50;
+				sam.Health_Potion = false;
+				label1.Text = Convert.ToString(sam.Health);
+			}
+			else
+			{
+				if (stor < 10)
+				{
+					Story.Texts(label1);
+					stor += 1;
+				}
+				else if (stor == 10)
+				{
+					Story.Texts2(label1);
+				}
+			}
+		}
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+	public class Creature
+	{
+		public int Power;
+		public int Health;
+		public bool Health_Potion;
+		public int attack(int damage_coefficent = 1)
+		{
+			Random rast = new Random();
+			int hasar = rast.Next(1*damage_coefficent, 10*damage_coefficent);
+			return hasar;
+		}
+	}
+	public class Story
+	{
+		public static string Texts(Label label)
+		{
+			label.Text = "ÝKSÝRÝN KALMADI";
+			return label.Text;
+		}
+		public static string Texts2(Label label)
+		{
+			label.Text = "ÝKSÝRÝN KALMADI!!!";
+			return label.Text;
+		}
+	}
+}
